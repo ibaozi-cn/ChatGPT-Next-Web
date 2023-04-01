38bb6ead223b7cb2d51424c3124b38152dba16e2
校长 <zzy0523@gmail.com>
2023-04-01 12:28:39 +0800

    还原logo设计，增加圆角

--- app/components/home.tsx
+++ app/components/home.tsx
@@ -36,6 +36,7 @@ import dynamic from "next/dynamic";
 import { REPO_URL } from "../constant";
 import { ControllerPool } from "../requests";
 import { Prompt, usePromptStore } from "../store/prompt";
+import Image from "next/image";
 
 export function Loading(props: { noLogo?: boolean }) {
   return (
@@ -669,7 +670,11 @@ export function Home() {
             </div>
             <div className={styles["sidebar-action"]}>
               <a href={REPO_URL} target="_blank">
-                bagutree
+                <img
+                  className={styles["rounded-img"]}
+                  src={"/bagutree-logo.jpg"}
+                  alt="My Image"
+                />
               </a>
             </div>
           </div>
